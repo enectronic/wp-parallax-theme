fb32afd7035b0d0f24ab97894f6e43ea9ebeb533
Johan Ljung <jollelj@hotmail.com>
2015-05-18 13:33:33 +0200

    Removed some unused libraries

--- functions.php
+++ functions.php
@@ -5,9 +5,6 @@
 function theme_scripts() {
 	wp_enqueue_style( 'Main CSS', get_stylesheet_uri() );
 	wp_enqueue_script( 'jQuery', get_template_directory_uri() . '/bower_components/jquery/dist/jquery.min.js', array(), '1.0.0', true );
-	wp_enqueue_script( 'TweenMax', get_template_directory_uri() . '/bower_components/gsap/src/minified/TweenMax.min.js', array(), '1.0.0', true );
-	wp_enqueue_script( 'ScrollMagic', get_template_directory_uri() . '/bower_components/ScrollMagic/scrollmagic/minified/ScrollMagic.min.js', array(), '1.0.0', true );
-	wp_enqueue_script( 'Anim', 'https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.3/plugins/animation.gsap.min.js', array(), '1.0.0', true);
 	wp_enqueue_script( 'Main js', get_template_directory_uri() . '/js/min/main-min.js', array(), '1.0.0', true );
 }
 
