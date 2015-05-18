58cdca2b649c35235c853401665004d785393838
Johan Ljung <jollelj@hotmail.com>
2015-05-18 15:30:28 +0200

    Footer mock completed

--- footer-front-page.php
+++ footer-front-page.php
@@ -1,4 +1,9 @@
-  <p>Yo, i'm the front-page footer</p>
+  <footer class="footer-front-page">
+    <div class="footer-front-page__content">
+      <h2 class="footer-front-page__heading">Kontakt</h2>
+      <p class="footer-front-page__text">Om du vill komma i kontakt med någon av oss går det bra att höra av sig direkt till behörig person via mail eller telefon. Om du har en generell fråga eller har en offertförfrågan kan du skicka ett mail till <span class="footer-front-page__highlight">kontakt@lsentertainment.se</span> eller ringa till kontoret på telefonnummer <span class="footer-front-page__highlight">0221-238 00</span></p>
+    </div>
+  </footer>
   <?php wp_footer(); ?>
   </body>
 </html>
